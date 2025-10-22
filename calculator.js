fd03d39778d5fa66053b33845286c5c0dfe0637f
Abderrahman <brissiabderrahman@gmail.com>
2025-10-22 14:44:58 +0100

    premier commit calculator

---
+++ calculator.js
@@ -0,0 +1,24 @@
+class Calculator {
+    add(a, b) {
+        return a + b;
+    }
+    
+    subtract(a, b) {
+        return a - b;
+    }
+    
+    multiply(a, b) {
+        return a * b;
+    }
+    
+    power(a) {
+        return a * a;
+    }
+
+    // divide(a, b) {
+    //     if (b === 0) {
+    //         throw new Error("Division by zero!");
+    //     }
+    //     return a / b;
+    // }  
+}
